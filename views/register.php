560b7dd1d751ddf4c6012b01ce4ac86be4e53527
Farzin Bidokhti <farzin.bidokhti@gmail.com>
2024-03-03 10:50:25 +0330

    Registration started

---
+++ views/register.php
@@ -0,0 +1,35 @@
+<h1>Create an account</h1>
+
+<form action="" method="post">
+  <div class="row">
+    <div class="col">
+    <div class="mb-3">
+    <label class="form-label">First name</label>
+    <input type="text" name="firstname" class="form-control">
+  </div>
+    </div>
+    <div class="col">
+    <div class="mb-3">
+    <label class="form-label">Last name</label>
+    <input type="text" name="lastname" class="form-control">
+  </div>
+    </div>
+  </div>
+
+  <div class="mb-3">
+    <label class="form-label">Email</label>
+    <input type="text" name="email" class="form-control">
+  </div>
+
+  <div class="mb-3">
+    <label class="form-label">Password</label>
+    <input type="password" name="password" class="form-control">
+  </div>
+
+  <div class="mb-3">
+    <label class="form-label">Confirm password</label>
+    <input type="password" name="confirm_password" class="form-control">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
